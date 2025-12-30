1fea003d47c7aae7df5cffb678cb53c8b93ad325
ogene70 <ogene.joseph@etu.iut-tlse3.fr>
2025-12-30 19:34:47 +0100

    first test passed

--- tests/test_add_resource.py
+++ tests/test_add_resource.py
@@ -5,9 +5,11 @@ def test_example():
     #SET UP TO INITIALISE VARIABLES
     a: int = 9
     b: int = 8 
+    result_to_test: float = a+b
     #ACTION PART TO LAUNCH THE SCRIPT TO TEST
      
     #ASSERT PART TO VERIFY A RESULT
-    with pytest.raises(ValueError) as excinfo:
-        result_to_test: float = a+b
-    assert "les nombres à calculer doivent être des nombres entiers" in str(excinfo.value)
+    assert type(result_to_test)==int
+    # with pytest.raises(TypeError) as excinfo:
+    #     result_to_test == float
+    # assert "les nombres à calculer doivent être des nombres entiers" in str(excinfo.value)
